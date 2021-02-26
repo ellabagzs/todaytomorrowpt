31f8877cbb087c4a41e18a69ed15ed278791ae21
ellabagzs <76996927+ellabagzs@users.noreply.github.com>
2021-02-27 00:44:39 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head> <title> MATH </title> </head>
+<body>
+
+<p align="center">
+<body background="aesthetic.jpeg3.jpg">
+
+<p align="center">
+<font color="light grey", size="10", face="Times New Roman">
+<br>
+<b> <i> TOMORROW </i> </b> </font></center>
+<p align="center">
+<img src="tlemathpt.jpej", width="900", length="900"> </img>
+<p align="center">
+<font color="white", size="5", face="Times New Roman">
+the bird symbolizes the hope to continue
+<br>
+despite the hardships and problems that not just in this
+<br>
+time of pandemic but in our whole ebtirelife as human beings.
+<br>
+it also like a bird 
+<br>
+that senses the dawn and carefully starts to sing while it is still dark
+<br>
+be patience, don't loose hope in yourself always 
+<br>
+holdback and trust the process 
+<br>
+everythings gonna be fine keep moving and thrieve for the best. </font></center>
+<br>
+<iframe width="560" height="315" src="https://youtu.be/_PiEdR0Bw70
+XMA Header Image
+MATH & TLE PT
+youtube.com" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+</body>
+</html>
